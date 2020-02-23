d21b76ad2434cbb0bb0b5115ca70dc6c062db112
sai1004 <saikiran.bavandla@gmail.com>
2020-02-23 12:04:01 +0530

    init commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "./App.css";
+import Todos from "./components/Todos";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Todos />
+        <p>hello</p>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Todos.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Todos extends Component {
+  render() {
+    return (
+      <div>
+        <h1> hey hello buddy, I'm Todo! </h1>
+      </div>
+    );
+  }
+}
+
+export default Todos;
